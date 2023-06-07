534f300a794c43ed45a4e5a5b775513cfb4cfbb4
mamunuddinbu <mamun.bbn.bd@gmail.com>
2023-06-07 07:28:00 +0600

    Basic folder and file created

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
+import Main from "./layout/Main";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Main></Main>,
   },
 ]);
 
---
+++ src/components/common/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <p>nav compo</p>
+        </div>
+    );
+};
+
+export default Navbar;
