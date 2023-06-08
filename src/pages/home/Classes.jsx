bf05616a56e1be2799303b6923d4aa5ef4fbb393
mamunuddinbu <mamun.bbn.bd@gmail.com>
2023-06-08 16:58:51 +0600

    Classes Page completed

--- src/pages/home/Classes.jsx
+++ src/pages/home/Classes.jsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Classes = () => {
-    return (
-        <div>
-            class compo
+  const [classes, setClasses] = useState([]);
+
+  useEffect(() => {
+    const fetchClasses = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/classes');
+        setClasses(response.data);
+      } catch (error) {
+        console.error('Error fetching classes:', error);
+      }
+    };
+
+    fetchClasses();
+  }, []);
+
+  return (
+    <div className='grid grid-cols-3 gap-3 mt-4'>
+      {classes.map((classItem) => (
+        <div key={classItem.id} className={`card ${classItem.availableSeats === 0 ? 'bg-red-500 p-3' : 'bg-green-200 p-3'}`}>
+          {/* <img src={classItem.image} alt={classItem.name} className="rounded-xl" /> */}
+          <img src="https://media.gettyimages.com/id/1128725181/photo/senior-teacher-talking-to-large-group-of-college-students-in-amphitheater.jpg?s=1024x1024&w=gi&k=20&c=X7c7R5RB8CIK84JIeaMC_SuAznEHs2tjiCNamrpc7Ts=" alt="" />
+          <div className="card-body">
+            <h2 className="card-title">{classItem.name}</h2>
+            <p>Instructor: {classItem.instructor}</p>
+            <p>Available Seats: {classItem.availableSeats}</p>
+            <p>Price: {classItem.price}</p>
+            {classItem.availableSeats === 0 ? (
+              <p>Class is full</p>
+            ) : (
+              <button disabled={false} className="btn btn-primary">
+                Select
+              </button>
+            )}
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default Classes;
--- src/pages/home/Instructors.jsx
+++ src/pages/home/Instructors.jsx
@@ -22,11 +22,14 @@ const Instructors = () => {
       {instructors.map((instructor) => (
         <div key={instructor._id} className="card w-80 m-3 bg-base-100 shadow-xl">
           <figure className="px-10 pt-10">
-            <img src={instructor.photo_url} alt={instructor.name} className="rounded-xl" />
+            {/* <img src={instructor.photo_url} alt={instructor.name} className="rounded-xl" /> */}
+            <img src="https://img.freepik.com/free-photo/young-handsome-man-holding-notebooks-concept-e-learning-courses_1258-26588.jpg?w=996&t=st=1686208320~exp=1686208920~hmac=6888ec78127889c322d9fa95dab4606b3fcc4d15678b4e5f656f79e22733b46d" alt="" />
           </figure>
           <div className="card-body items-center text-center">
             <h2 className="card-title">{instructor.name}</h2>
             <p>Email: {instructor.email}</p>
+            <p>Role: {instructor.role}</p>
+            
             <div className="card-actions">
               <button className="btn btn-primary">View Profile</button>
             </div>
